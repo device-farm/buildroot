e2b77d1e5e98d9519d4c2ad49eaa88b6ad75aa83
Pavel Burgr <pb@drake.cz>
2021-01-11 19:15:22 +0100

    WIP

--- package/device-farm/device-farm.mk
+++ package/device-farm/device-farm.mk
@@ -13,9 +13,15 @@ define DEVICE_FARM_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(DEVICE_FARM_PKGDIR)/etc-default-dockerd $(TARGET_DIR)/etc/default/dockerd
 	$(INSTALL) -D -m 0744 $(DEVICE_FARM_PKGDIR)/etc-init.d-S50df-wireguard $(TARGET_DIR)/etc/init.d/S50df-wireguard
 
-	ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT),y)
-		$(INSTALL) -D -m 0744 $(DEVICE_FARM_PKGDIR)/etc-init.d-S45df-wifi $(TARGET_DIR)/etc/init.d/S45df-wifi
-	endif
+	#TODO make this optional, the block bellow does not work
+	$(INSTALL) -D -m 0744 $(DEVICE_FARM_PKGDIR)/etc-init.d-S45df-wifi $(TARGET_DIR)/etc/init.d/S45df-wifi
 endef
+
+#ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT),y)
+#define DEVICE_FARM_INSTALL_WIFI
+#	$(INSTALL) -D -m 0744 $(DEVICE_FARM_PKGDIR)/etc-init.d-S45df-wifi $(TARGET_DIR)/etc/init.d/S45df-wifi
+#endef
+#DEVICE_FARM_INSTALL_TARGET_CMDS += DEVICE_FARM_INSTALL_WIFI
+#endif
 
 $(eval $(virtual-package))
